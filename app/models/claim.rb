ef57968ddb92af08617a1b6b5a358a51f159d630
CaptConrado <ottey001@gmail.com>
2014-03-25 14:00:10 -0700

    added some logic to the claim model

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -2,8 +2,9 @@ class Claim < ActiveRecord::Base
   self.primary_key = "claim_id"
   belongs_to :video, foreign_key: 'ytube_id'
   
-  def oy
-    # kay = video.ytube_id
-    # Claim.where('ytube_id == #{kay}').pluck(:claim_id)
+  def all_claims
+    video
   end
 end
+
+# Claim.where('ytube_id == #{kay}').pluck(:claim_id)
