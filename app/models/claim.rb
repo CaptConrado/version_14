800d898266b0fd3cce0254a973e10c0ee2aeea81
CaptConrado <ottey001@gmail.com>
2014-03-26 18:12:23 -0700

    added claim csv

--- app/models/claim.rb
+++ app/models/claim.rb
@@ -9,15 +9,60 @@ class Claim < ActiveRecord::Base
     video
   end
 
+  def id_check name
+    if name =~ /Alabama/
+      2
+    elsif name =~ /Amymarie/
+      3
+    elsif name =~ /Austin/
+      4
+    elsif name =~ /BarkleyThePom/
+      5
+    elsif name =~ /Bottlerocket/
+      6
+    elsif name =~ /Brandon Calvillo/
+      7
+    elsif name =~ /Brennan/
+      8
+    elsif name =~ /Furlan/
+      9
+    elsif name =~ /Devlin/
+      11
+    # elsif name =~ /Amymarie/
+    # #   3
+    # elsif name =~ /Amymarie/
+    # #   3
+    # elsif name =~ /Amymarie/
+    # #   3
+    # elsif name =~ /Amymarie/
+    # #   3
+    # elsif name =~ /Amymarie/
+    # #   3
+    # elsif name =~ /Amymarie/
+    # #   3
+    # elsif name =~ /Amymarie/
+    # #   3
+    # elsif name =~ /Amymarie/
+    # #   3
+    # elsif name =~ /Amymarie/
+    #   3
+    else
+      puts "no match"
+    end
+  end
+
+# puts id_check "alm"
+
 
   def self.import file
-    cid = 10
+
     CSV.foreach(file.path, :headers => true, :encoding => 'windows-1251:utf-8') do |row|
+          
       Claim.create(
         # title:           row[3],
         ytube_id:        row[1],
         claim_id:        row[0],
-        producer_id:     cid
+        producer_id:     16
       )
     end
   end
